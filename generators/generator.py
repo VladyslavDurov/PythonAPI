93f662abd038169218ba7c27b9992691b2245643
Vlad Durov <durov.vlad@gmail.com>
2016-06-18 18:48:53 +0300

    :)

--- generators/generator.py
+++ generators/generator.py
@@ -4,16 +4,16 @@ mygenerator = (x**2 for x in range(3,5))
 
 #variant_2
 def createGenerator():
-	for item in range(2,5):
-		yield item**3
+    for item in range(2,5):
+        yield item**3
 
 for item in mygenerator:
-	print(item)
+    print(item)
 
 print(20*'+')
 
 for x in createGenerator():
-	print(x)
+    print(x)
 
 print(20*'+')
 
