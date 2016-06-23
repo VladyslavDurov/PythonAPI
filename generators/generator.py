03e0cd5794b0307f5a5758d63219f655c945472b
VladyslavDurov <durov.vlad@gmail.com>
2016-06-23 16:12:39 +0300

    add variant generator

--- generators/generator.py
+++ generators/generator.py
@@ -1,23 +1,38 @@
+from functools import wraps
+
+def decor(func):
+    @wraps(func)
+    def wrapper(*args):
+        func()
+        print(20*'+')
+    return wrapper
 
 #variant_1
-mygenerator = (x**2 for x in range(3,5))
+@decor
+def createGenerator1():
+    mygenerator = (x**2 for x in range(3,5))
+    for item in mygenerator:
+        print(item)
 
 #variant_2
-def createGenerator():
-    for item in range(2,5):
-        yield item**3
+@decor
+def createGenerator2():
+    def generator():
+        for item in range(2,5):
+            yield item**3
+    for x in generator():
+        print(x)
 
-for item in mygenerator:
-    print(item)
+#variant_3
+def createGenerator3():
+    a = [1,2,3,5]
+    my_generator = iter(a)
+    for item in my_generator:
+        print(item)
 
-print(20*'+')
+createGenerator1()
+createGenerator2()
+createGenerator3()
 
-for x in createGenerator():
-    print(x)
 
-print(20*'+')
 
-generator = createGenerator()
-print(next(generator))
-print(next(generator))
-print(next(generator))
